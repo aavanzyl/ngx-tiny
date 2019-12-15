8663e1d8f84779bda6f88d82136a3267f8ddd7d6
Anton Van Zyl <aavanzyl@gmail.com>
2019-12-15 08:28:45 +0200

    WIP - Date Picker

--- src/app/project/date-picker/date-picker.component.ts
+++ src/app/project/date-picker/date-picker.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-date-picker',
   templateUrl: './date-picker.component.html',
-  styleUrls: ['./date-picker.component.scss']
+  styleUrls: ['./date-picker.component.scss'],
+  preserveWhitespaces: true
 })
 export class DatePickerComponent implements OnInit {
 
